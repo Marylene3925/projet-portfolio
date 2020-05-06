83b468299b13a40097a31ca0e619ab4be9fc0f3d
Marylene3925 <58249807+Marylene3925@users.noreply.github.com>
2020-05-06 12:54:57 +0200

    creation de la partie projet

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -13,4 +13,59 @@ class PortfolioController extends AbstractController
     {
         return $this->render('Portfolio/index.html.twig');
     }
+    
+    public function navbar()
+    {
+        return $this->render('Portfolio/navbar.html.twig');
+    }
+
+    public function bootstrap()
+    {
+        return $this->render('Portfolio/bootstrap.html.twig');
+    }
+    public function javascript()
+    {
+        return $this->render('Portfolio/javascript.html.twig');
+    }
+    
+    public function php()
+    {
+        return $this->render('Portfolio/php.html.twig');
+    }
+
+    public function symfony()
+    {
+        return $this->render('Portfolio/symfony.html.twig');
+    }
+
+    public function wordpress()
+    {
+        return $this->render('Portfolio/wordpress.html.twig');
+    }
+
+    public function projet_bootstrap()
+    {
+        return $this->render('Portfolio/projets/projet_bootstrap.html.twig');
+    }
+
+    public function projet_javascript()
+    {
+        return $this->render('Portfolio/projets/projet_javascript.html.twig');
+    }
+
+    public function projet_php()
+    {
+        return $this->render('Portfolio/projets/projet_php.html.twig');
+    }
+
+    public function projet_symfony()
+    {
+        return $this->render('Portfolio/projets/projet_symfony.html.twig');
+    }
+
+    public function projet_wordpress()
+    {
+        return $this->render('Portfolio/projets/projet_wordpress.html.twig');
+    }
+
 }
