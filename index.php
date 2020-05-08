c756a1e30b039bd0e87c3ac50d32879bb1137d15
Marylene3925 <58249807+Marylene3925@users.noreply.github.com>
2020-05-08 21:49:46 +0200

    version php

---
+++ index.php
@@ -0,0 +1,225 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link href=" https://bootswatch.com/4/journal/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="app.css">
+<div class="menu"> 
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light " id=menu>
+        <button id="btn_nav" class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            
+        </button><div class=" mot-menu ">menu</div>
+        <div class="navbar-collapse.show collapse show link_menu" id="navbarNavAltMarkup">
+            <ul class="navbar-nav d-flex flex-column ">
+                <a class="nav-item nav-link  " href="#présentation">Présentation</a>
+                <a class="nav-item nav-link " href="#compétences">Compétences</a>
+                <a class="nav-item nav-link " href="#réalisations">Réalisations</a>
+                <a class="nav-item nav-link " href="#parcours">Parcours</a>
+                <a class="nav-item nav-link " href="#contact">contact</a>
+            </ul>
+        </div>
+
+        
+    </nav>
+
+
+
+<div class="container p-0">
+
+    <div class="container-nav">
+        <nav class="d-flex justify-content-between">
+            <div class="font-weight-bolder nav_contact"><a href="#header">Marylène Mougeot</a></div>
+
+            <div class="nav_contact_mail "><a href="#">me contacter<img
+                        src="https://img.icons8.com/carbon-copy/100/000000/important-mail.png" class="img-mail "></a>
+            </div> 
+        </nav>
+    </diV>
+
+    <div class="scrolling-box">
+
+        <div class="partie-header border-bottom border-dark d-flex justify-content-between" id="header">
+
+
+        </div>
+
+
+        <div class="partie-presentation border-bottom border-dark " id="présentation">
+
+            <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>à propos de moi <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+
+            <div class="col- col-sm- col-md- col-lg- col-xl-10 d-flex justify-content-end ">
+                <p class="fts4 mt-4 text-presentation mt-5">
+                    Je suis Marylène,<br> développeuse web junior
+                </p>
+            </div>
+
+            <div class=" d-flex justify-content-around pt-5 pb-7">
+
+                <div class="col- col-sm- col-md- col-lg- col-xl-2">
+                    <img src="image/moi.png" alt="photo de moi" class="logo-img mt-5">
+                </div>
+                <div class="col- col-sm- col-md- col-lg- col-xl-5">
+                    <p class="pt-9 mt-4 fts2 text-left biographie fts5 ">
+                        Je suis actuellement en formation "Développeur Web & Web mobile" depuis Novembre 2019.
+                        <br>
+                        Ce qui me plaît dans le développement c'est la conception graphique, la découverte de nouvelles
+                        technologies web.
+                        <br>
+                        Je me passionne pour la décoration de bougies et la cuisine.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="partie-presentation border-bottom border-dark" id="compétences">
+
+            <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>Mes compétences <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+
+            <div class="d-flex justify-content-around mt-5 pb-7">
+                <div class="mt-5">
+                    <p class="fts2 border-bottom border-dark">Languages</p>
+                    <div>
+                        <div class="d-flex">
+                            <p> HTML5 / CSS3 :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /></p>
+                        </div>
+
+                        <div class="d-flex">
+                            <p> JAVASCRIPT :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+
+                        <div class="d-flex">
+                            <p> PHP :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5">
+                    <p class="fts2 border-bottom border-dark">Framework</p>
+                    <div>
+                        <div class="d-flex">
+                            <p> BOOTSTRAP :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+
+                        <div class="d-flex">
+                            <p> SYMFONY ::</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+
+                        <div class="d-flex">
+                            <p> REACT :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5">
+                    <p class="fts2 border-bottom border-dark">CMS</p>
+                    <div>
+                        <div class="d-flex">
+                            <p> WORDPRESS :</p>
+                            <p><img src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/material-rounded/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /><img
+                                    src="https://img.icons8.com/small/24/000000/star.png" /></p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="partie-réalisations  border-bottom border-dark  pb-7 " id="réalisations">
+
+            
+            <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>Mes réalisations <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+
+            <div class=" d-flex justify-content-around pt-7" id="bootstrap">
+            <?php include 'bootstrap.php';?>
+            </div>
+
+        </div>
+
+
+
+        <div class="partie-parcours border-bottom border-dark " id="parcours">
+            <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>à propos de moi <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+        </div>
+
+
+
+        <div class="partie-contact  " id="contact">
+            <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>à propos de moi <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script type="text/javascript">$('select').formSelect();</script>
+
+</body>
+</html>
---
+++ wordpress.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link href=" https://bootswatch.com/4/journal/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="app.css">
+<div class="menu"> 
+</head>
+<body>
+
+<div class="menu">   
+
+    <nav class="navbar navbar-expand-lg navbar-light " id=menu>
+        <button id="btn_nav" class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse.show collapse show link_menu" id="navbarNavAltMarkup">
+            <ul class="navbar-nav d-flex flex-column ">
+                <a class="nav-item nav-link  " href="#présentation">Présentation</a>
+                <a class="nav-item nav-link " href="#compétences">Compétences</a>
+                <a class="nav-item nav-link " href="#réalisations">Réalisations</a>
+                <a class="nav-item nav-link " href="#parcours">Parcours</a>
+                <a class="nav-item nav-link " href="#contact">contact</a>
+            </ul>
+        </div>
+
+        <div class="d-flex mot-menu">menu</div>
+    </nav>
+</div>
+
+<div class="container p-0">
+
+    <div class="scrolling-box">    
+ 
+         <div class="d-flex justify-content-center text-uppercase font-weight-bold fts3 mb-2 pt-5 title-partie">
+                <p>Mes réalisations <br> <span class="d-flex justify-content-center word-spacing-1"> &#47; &#47; &#47;
+                    </span> </p>
+            </div>
+
+        <div class=" d-flex justify-content-around pt-7">
+
+            <div class="col- col-sm- col-md- col-lg- col-xl-2">
+                <p class="fts5 mt-4 "><span class="border-bottom border-dark">symfony</span></p>
+
+                <p class="fts5 pt-7 "><a href="{{ path('projet_symfony') }}">Mes projets</a></p>
+            </div>
+
+            <div class="col- col-sm- col-md- col-lg- col-xl-6">
+                <p class="fts6 mt-4 text-center">Quelques aperçus de mes projets</p>
+                <div class="slider-wrapper">
+                    <div class="slider-item blueasy">
+                        <img src="image/fantaisy.jpg" class="img-slider  border border-dark">
+                    </div>
+                   
+                </div>
+                <nav class="slider-nav text-center m-0 p-0">
+                    <button class="slider-previous">
+                        Précédent
+                    </button>
+                    <button class="slider-next">
+                        Suivant
+                    </button>
+                    <button class="slider-restart">
+                        Reprendre
+                    </button>
+                </nav>
+            </div>
+
+
+            <div class="d-flex justify-content-end pt-7">
+                <div class="nav-competence">
+                    <div class=" d-flex justify-content-end fts5 ">
+                        <button class="button-bootstrap"><a href="{{ path('bootstrap') }}"><span>B</span></a></button>
+                    </div>
+
+                    <div class=" d-flex justify-content-end fts5">
+                        <button class="button-javascript"><a href="{{ path('javascript') }}"><span>J</span></a></button>
+                    </div>
+
+                    <div class=" d-flex justify-content-end fts5">
+                        <button class="button-php"><a href="{{ path('php') }}"><span>P</span></a></button>
+                    </div>
+
+                    <div class=" d-flex justify-content-end fts5">
+                        <button class="button-symfony"><a href="{{ path('symfony') }}"><span>S</span></a></button>
+                    </div>
+
+                    <div class=" d-flex justify-content-end fts5">
+                        <button class="button-wordpress"><a href="{{ path('wordpress') }}"><span>W</span></a></button>
+                    </div>
+                </div>
+            </div>   
+        </div>
+    </div>    
+</div>
+ 
+
+<script>
+
+    let prevBtn = document.querySelector('.slider-previous');
+    let nextBtn = document.querySelector('.slider-next');
+    let sliderWrapper = document.querySelector('.slider-wrapper')
+    let restart = document.querySelector('.slider-restart')
+    let slide = document.querySelectorAll('.slider-item');
+    let sliderLength = slide.length;
+    let counter = 0;
+    let delay = setInterval(sliding, 1000)
+
+    slide[counter].classList.add('active');
+
+    //BTN SUIVANT
+    nextBtn.addEventListener('click', () => {
+        clearInterval(delay);
+        slide[counter].classList.remove('active');
+        counter++;
+        if (counter >= sliderLength) {
+            counter = 0;
+        }
+        slide[counter].classList.add('active');
+    });
+
+    //BTN PRECEDENT
+    prevBtn.addEventListener('click', () => {
+        clearInterval(delay);
+        slide[counter].classList.remove('active');
+        counter--;
+        if (counter < 0) {
+            counter = sliderLength - 1;
+        }
+        slide[counter].classList.add('active');
+    });
+
+    //FUNCTION AUTOSLIDE
+    function sliding() {
+        slide[counter].classList.remove('active');
+        counter++;
+        if (counter >= sliderLength) {
+            counter = 0;
+        }
+        slide[counter].classList.add('active');
+    }
+
+    //SURVOL
+    sliderWrapper.addEventListener('mouseenter', () => {
+        clearInterval(delay);
+    })
+
+    //ON QUITTE LE SURVOL
+    sliderWrapper.addEventListener('mouseleave', () => {
+        delay = setInterval(sliding, 1000)
+    })
+
+    //BTN RESTART
+    restart.addEventListener('click', () => {
+        delay = setInterval(sliding, 1000)
+    })
+</script>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script type="text/javascript">$('select').formSelect();</script>
+
+</body>
+</html>
